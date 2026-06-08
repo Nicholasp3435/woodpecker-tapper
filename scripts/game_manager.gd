f7ffb0974c64ffaa61c4952e07d22ca74b5ab35a
Nicholas <nap44794@uga.edu>
2026-06-07 21:05:25 -0400

    fix: use new AudioStreamPlayers to play strike audio

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,7 +2,9 @@ extends Node
 
 
 @onready var pyre: Node2D = $"../Pyre";
-@onready var audio_player: AudioStreamPlayer2D = $"../AudioStreamPlayer2D"
+
+const WOODPECKER_DATA: String = "res://assets/woodpecker_data.json";
+const WOODPECKER_STRIKE: String = "res://assets/strike.mp3";
 
 var in_sample_mode: bool = false;
 var in_recording_mode: bool = false;
@@ -13,11 +15,12 @@ var cur_state: int = Game_States.TIMER;
 var strike_count: int = 0;
 
 var woodpecker_data: Dictionary = get_data();
+var woodpecker_strike: AudioStreamMP3 = load_audio();
 
 func _input(event: InputEvent) -> void:
 	if not in_sample_mode:
 		if event.is_action_pressed("strike") and pyre.mouse_in:
-			pyre.animated_sprite.play("strike")
+			pyre.animated_sprite.play("strike");
 			handle_strike(1.0);
 		elif event.is_action_released("strike"):
 			if pyre.mouse_in:
@@ -27,10 +30,21 @@ func _input(event: InputEvent) -> void:
 
 func handle_strike(volume: float):
 	strike_count += 1;
+	
+	pyre.animated_sprite.play("strike");
+	var audio_player: AudioStreamPlayer = AudioStreamPlayer.new();
+	add_child(audio_player);
+	var stream: AudioStream = woodpecker_strike;
+	audio_player.stream = stream;
 	audio_player.volume_linear = volume;
 	audio_player.play();
-
-	print("strike: ", strike_count, " volume: ", volume);
+	audio_player.finished.connect(audio_player.queue_free);
+	
+func load_audio() -> AudioStreamMP3:
+	var file: FileAccess = FileAccess.open(WOODPECKER_STRIKE, FileAccess.READ);
+	var sound: AudioStreamMP3 = AudioStreamMP3.new();
+	sound.data = file.get_buffer(file.get_length());
+	return sound;
 	
 func handle_pattern(data: Dictionary):
 	for strike in data.data:
@@ -39,7 +53,7 @@ func handle_pattern(data: Dictionary):
 		get_tree().create_timer(time).timeout.connect(func(): handle_strike(volume));
 
 func get_data() -> Dictionary:
-	var woodpecker_file: String = FileAccess.get_file_as_string("res://assets/woodpecker_data.json");
+	var woodpecker_file: String = FileAccess.get_file_as_string(WOODPECKER_DATA);
 	
 	var json := JSON.new();
 	var error := json.parse(woodpecker_file);
